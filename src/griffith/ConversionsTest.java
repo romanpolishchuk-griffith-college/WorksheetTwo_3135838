afc4beaa511b8cdfa11fea7e43814bb7d6201d8d
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-03 23:55:53 +0000

    Implement euroToDollarTest method

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -8,7 +8,17 @@ class ConversionsTest {
 
 	@Test
 	void euroToDollarTest() {
-		fail("Not yet implemented");
+		//Create grades class instance
+		Conversions conversions = new Conversions();
+		
+		//Test max is the first element
+		assertEquals(10.48, conversions.euroToDollar(10));
+		//Test max is the last element
+		assertEquals(5.24, conversions.euroToDollar(5));
+		//Test max is in the middle
+		assertEquals(0, conversions.euroToDollar(0));
+		//Test max is the negative number
+		assertEquals(0, conversions.euroToDollar(-999));
 	}
 	
 	@Test
