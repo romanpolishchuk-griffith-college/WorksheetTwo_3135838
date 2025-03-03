00102bbc6811868e27d562f6287349004399d2fd
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-03 23:33:23 +0000

    Add Conversions class stab methods

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,5 +1,20 @@
 package griffith;
 
 public class Conversions {
-
+	public double euroToDollar(double euro) {
+		return 0.0;
+	}
+	public double dollarToEuro(double dollar){
+		return 0.0;
+	}
+	public int stringToInteger (String val){
+		return 0;
+	}
+	public String integerToString (int val){
+		return "";
+	}
+	public String switchCase() // change uppercase to lowercase and vice versa
+	{
+		return "";
+	}
 }
