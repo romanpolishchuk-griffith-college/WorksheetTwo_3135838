5e715aa9d34e265d10b9942687e08aa98097dc68
Roman Polishchuk <romanpolishchuk2006@gmail.com>
2025-03-04 00:20:21 +0000

    Fix comment messages for tests

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -11,13 +11,13 @@ class ConversionsTest {
 		//Create grades class instance
 		Conversions conversions = new Conversions();
 		
-		//Test max is the first element
+		//Test euro is 10
 		assertEquals(10.48, conversions.euroToDollar(10));
-		//Test max is the last element
+		//Test euro is 5
 		assertEquals(5.24, conversions.euroToDollar(5));
-		//Test max is in the middle
+		//Test euro is 0
 		assertEquals(0, conversions.euroToDollar(0));
-		//Test max is the negative number
+		//Test euro is -999
 		assertEquals(0, conversions.euroToDollar(-999));
 	}
 	
@@ -26,13 +26,13 @@ class ConversionsTest {
 		//Create grades class instance
 		Conversions conversions = new Conversions();
 		
-		//Test max is the first element
+		//Test dollar is 9.432
 		assertEquals(9, conversions.dollarToEuro(9.432));
-		//Test max is the last element
+		//Test dollar is 102.704
 		assertEquals(98, conversions.dollarToEuro(102.704));
-		//Test max is in the middle
+		//Test dollar is 0
 		assertEquals(0, conversions.dollarToEuro(0));
-		//Test max is the negative number
+		//Test dollar is -999
 		assertEquals(0, conversions.dollarToEuro(-999));
 	}
 	
